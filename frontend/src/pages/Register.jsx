d04ad69c8a91b03306eaa64abbe9877b6915e646
unknown <hlxks2454@gmail.com>
2026-08-07 10:25:07 +0200

    Added Confirm Password

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -16,15 +16,24 @@ const Register = ({ setUser}) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (form.password !== form.confirmPassword){
+      setError("Password does not match");
+      return;
+    }
     try {
-      const res = await api.post("/api/auth/register", form);
-      setUser(res.data.user);
+      const res = await api.post("/api/auth/register", {
+        name: form.name,
+        email: form.email,
+        password: form.password,
+      });
+
+    setUser(res.data.user);
       navigate("/");
     } catch (err) {
-       
       setError("Registeration failed");
     }
-  }
+  };
 
   return (
   <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-100 to-blue-100">
@@ -57,7 +66,18 @@ const Register = ({ setUser}) => {
         className="border p-2 w-full mb-3" 
         value={form.password} onChange={(e) => setForm({...
         form, password: e.target.value})} />
-        <button className="bg-blue-500 text-white p-2 w-full">Login</button>
+        <input 
+          type="password"
+          placeholder="Confirm Password"
+          className="border p-2 w-full mb-3"
+          value={form.confirmPassword}
+          onChange={(e) =>
+            setForm({
+              ..form,
+              confirmPassword: e.target.value,
+            })
+            }  />
+        <button className="bg-blue-500 text-white p-2 w-full">Register</button>
               <p className="text-center mt-5 text-gray-600">
         Already have an account?{" "}
         <Link to="/login" className="text-blue-600 font-semibold hover:underline">
