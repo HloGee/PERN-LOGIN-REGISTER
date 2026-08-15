a61fd469742cf03dd6a873df74736376fc16fc85
unknown <hlxks2454@gmail.com>
2026-08-15 02:45:50 +0200

    Fix production CORS

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const cookieOptions = { 
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
 
