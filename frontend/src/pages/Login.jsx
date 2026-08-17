165249f6a277c47c8511ac85f93a19480b0fa42e
unknown <hlxks2454@gmail.com>
2026-08-17 09:45:37 +0200

    Fix normal login user state

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = ({ setUser}) => {
     e.preventDefault();
     try {
       const res = await api.post("/api/auth/login", form);
-      setUser(res.data);
+      setUser(res.data.user);
       navigate("/");
     } catch (err) {
        
